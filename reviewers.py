56068ea3f7c3690f2e062da5724fa74b1ac9db26
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-05-17 09:09:31 -1000

    Add reviewers for insertion to db

---
+++ reviewers.py
@@ -0,0 +1,505 @@
+reviewers = [
+    {
+        "name": "Karen Osborn",
+        "organization": "National Museum of Natural History (Smithsonian)",
+        "email": "OsbornK@si.edu",
+        "focus_group": "Annelid",
+        "focus_subgroup": "Annelids"
+    },
+    {
+        "name": "Mary Wicksten",
+        "organization": "Texas A&M University",
+        "email": "wicksten@bio.tamu.edu",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "Squat lobsters, crabs, shrimp, barnacles"
+    },
+    {
+        "name": "Enrique Macpherson",
+        "organization": "Centro de Estudios Avanzados de Blanes (CSIC)",
+        "email": "macpherson@ceab.csic.es",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "Lithodids, Galatheoidea"
+    },
+    {
+        "name": "Keiji Baba",
+        "organization": "Kumamoto University",
+        "email": "kbaba.kumamoto@gmail.com",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "Squat lobsters"
+    },
+    {
+        "name": "Bob Van Syoc",
+        "organization": "California Academy of Sciences",
+        "email": "bvansyoc@calacademy.org",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "barnacles"
+    },
+    {
+        "name": "Buz Wilson",
+        "organization": "Saugatuck Natural History Laboratory",
+        "email": "gdfw@SNHLab.com",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "isopods (Munnopsidae)"
+    },
+    {
+        "name": "Tom Hansknecht",
+        "organization": "Barry Vittor and Associates, Inc.",
+        "email": "tjhansk@comcast.net",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "Other arthropods (isopods)"
+    },
+    {
+        "name": "Marina Malyutina",
+        "organization": "Russian Academy of Sciences",
+        "email": "m_malyutina@mail.ru",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "Isopods"
+    },
+    {
+        "name": "Chris Boyko",
+        "organization": "American Museum of Natural History",
+        "email": "cboyko@amnh.org",
+        "focus_group": "Arthropods",
+        "focus_subgroup": "Isopods, parasites"
+    },
+    {
+        "name": "Scott France",
+        "organization": "University of Louisiana at Lafayette",
+        "email": "scott.france@louisiana.edu",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Isidids"
+    },
+    {
+        "name": "Les Watling",
+        "organization": "University of Hawaii",
+        "email": "watling@hawaii.edu",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Isidids, isopods, chrysogorgiids"
+    },
+    {
+        "name": "Tina Molodtsova",
+        "organization": "P.P. Shirshov Institute of Oceanology",
+        "email": "tina@ocean.ru",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Antipatharians, Soft corals, Pennatulaceans, coral associates"
+    },
+    {
+        "name": "Dennis Opresko",
+        "organization": "Oak Ridge National Laboratory",
+        "email": "dmopresko@hotmail.com",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "antiapatharia"
+    },
+    {
+        "name": "Asako Matsumoto",
+        "organization": "Chiba Institute of Technology",
+        "email": "amatsu@gorgonian.jp",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Acathogorgiids, Plexaurids, Primnoids"
+    },
+    {
+        "name": "Nadya Sanamyan",
+        "organization": "Kamchatka Branch of Pacific Institute of Geography",
+        "email": "actiniaria@sanamyan.com",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Actiniaria, tunicates"
+    },
+    {
+        "name": "Daniel Wagner",
+        "organization": "NOAA Papahānaumokuākea Marine National Monument",
+        "email": "dwagner@conservation.org",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Antipatharians"
+    },
+    {
+        "name": "Amy Baco-Taylor",
+        "organization": "Florida State University",
+        "email": "abacotaylor@fsu.edu",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Coralliids"
+    },
+    {
+        "name": "Allen Collins",
+        "organization": "National Museum of Natural History (Smithsonian)",
+        "email": "collinsa@si.edu",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "cnidaria, stenophora, porifera"
+    },
+    {
+        "name": "Phil Alderslade",
+        "organization": "CSIRO",
+        "email": "phil.alderslade@csiro.au",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "octocorallia"
+    },
+    {
+        "name": "Estefania Rodriguez",
+        "organization": "American Museum of Natural History",
+        "email": "erodriguez@amnh.org",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Actiniaria"
+    },
+    {
+        "name": "Abby LaPointe",
+        "organization": "University of Hawaii",
+        "email": "abbylap@hawaii.edu",
+        "focus_group": "Cnidarians",
+        "focus_subgroup": "Isidids"
+    },
+    {
+        "name": "Christopher Mah",
+        "organization": "National Museum of Natural History (Smithsonian)",
+        "email": "brisinga@gmail.com",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "Asteroids"
+    },
+    {
+        "name": "Andrey Gebruk",
+        "organization": "P.P.Shirshov Institute of Oceanology",
+        "email": "agebruk@gmail.com",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "holothuroids",
+        "notes": "CC Antonina"
+    },
+    {
+        "name": "Antonina Rogacheva",
+        "organization": "P.P. Shirshov Institute of Oceanology",
+        "email": "antoneene@gmail.com",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": ""
+    },
+    {
+        "name": "Charles Messing",
+        "organization": "Nova Southeastern University",
+        "email": "messingc@nova.edu",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "Feather stars"
+    },
+    {
+        "name": "Richard Mooi",
+        "organization": "California Academy of Sciences",
+        "email": "rmooi@calacademy.org",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "Urchins"
+    },
+    {
+        "name": "David Pawson",
+        "organization": "National Museum of Natural History (Smithsonian)",
+        "email": "PAWSOND@si.edu",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "holothuroids"
+    },
+    {
+        "name": "Alexander Mironov",
+        "organization": "P.P. Shirshov Institute of Oceanology",
+        "email": "miron@ocean.ru",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "Porcellanid seastars, stalked crinoids, echinoidea"
+    },
+    {
+        "name": "Doug Eernisse",
+        "organization": "University of California, Santa Cruz",
+        "email": "deernisse@fullerton.edu",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "seastars, Chitons, Limpets"
+    },
+    {
+        "name": "Marc Eleaume",
+        "organization": "Museum National D'Histoire Naturelle",
+        "email": "eleaume@mnhn.fr",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "asteroidea, crinoids"
+    },
+    {
+        "name": "Michel Roux",
+        "organization": "Museum National D'Histoire Naturelle",
+        "email": "mich.roux51@gmail.com",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "crinoids"
+    },
+    {
+        "name": "Tim O'Hara",
+        "organization": "Museum Victoria",
+        "email": "tohara@museum.vic.gov.au",
+        "focus_group": "Echinoderms",
+        "focus_subgroup": "Ophiuroids"
+    },
+    {
+        "name": "Karen Sanamyan",
+        "organization": "Kamchatka Branch of Pacific Institute of Geography",
+        "email": "karen@sanamyan.com",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "tunicates, actiniaria"
+    },
+    {
+        "name": "Pamela Neubert",
+        "organization": "Woods Hole Oceanographic Institution",
+        "email": "plneubert@gmail.com",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "Aplacophora, molluscs"
+    },
+    {
+        "name": "Kevin Kocot",
+        "organization": "University of Alabama",
+        "email": "kmkocot@ua.edu",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "aplacophoran molluscs"
+    },
+    {
+        "name": "Jessica Goodheart",
+        "organization": "National Museum of Natural History (Smithsonian)",
+        "email": "GoodheartJ@si.edu",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "nudibranchia"
+    },
+    {
+        "name": "Terry Gosliner",
+        "organization": "",
+        "email": "tgosliner@calacademy.org",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "nudibranchia"
+    },
+    {
+        "name": "Julia Sigwart",
+        "organization": "Queen's University Belfast",
+        "email": "jsigwart@qub.ac.uk",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "chitons"
+    },
+    {
+        "name": "Robert Moffitt",
+        "organization": "NOAA Pacific Islands Fisheries Science Center",
+        "email": "",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "fish, crustaceans"
+    },
+    {
+        "name": "Claudia Mills",
+        "organization": "University of Washington",
+        "email": "cemills@uw.edu",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "ctenophores, jellies"
+    },
+    {
+        "name": "Erik V. Thuesen",
+        "organization": "The Evergreen State College",
+        "email": "thuesene@evergreen.edu",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "ctenophores"
+    },
+    {
+        "name": "Steven Haddock",
+        "organization": "MBARI",
+        "email": "haddock@mbari.org",
+        "focus_group": "Other Invertebrates"
+    },
+    {
+        "name": "Matthew R. L. Jones",
+        "organization": "Auckland University of Technology",
+        "email": "matthew.jones@aut.ac.nz",
+        "focus_group": "Other Invertebrates"
+    },
+    {
+        "name": "Michael Vecchione",
+        "organization": "National Museum of Natural History (Smithsonian)",
+        "email": "vecchiom@si.edu",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "cephalopods"
+    },
+    {
+        "name": "George Matsumoto",
+        "organization": "MBARI",
+        "email": "mage@mbari.org",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "ctenophores, cnidaria"
+    },
+    {
+        "name": "Carole Hickman",
+        "organization": "University of California, Berkeley",
+        "email": "caroleh@berkeley.edu",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "gastropoda"
+    },
+    {
+        "name": "Megan McCuller",
+        "organization": "North Carolina Museum of Natural Sciences",
+        "email": "megan.mcculler@naturalsciences.org",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "Bryozoa"
+    },
+    {
+        "name": "Harlan Dean",
+        "organization": "Museum of Comparative Zoology, Harvard University",
+        "email": "Harlan.Dean@umb.edu",
+        "focus_group": "Other Invertebrates",
+        "focus_subgroup": "Sipuncula specimens, Echiura, and annelids"
+    },
+    {
+        "name": "Konstantin Tabachnick",
+        "organization": "P.P. Shirshov Institute of Oceanology",
+        "email": "tabachnick@mail.ru",
+        "focus_group": "Sponges",
+        "focus_subgroup": "glass sponges"
+    },
+    {
+        "name": "Henry Reiswig",
+        "organization": "University of Victoria",
+        "email": "hmreiswig@shaw.ca",
+        "focus_group": "Sponges",
+        "focus_subgroup": "glass sponges"
+    },
+    {
+        "name": "Rob van Soest",
+        "organization": "Naturalis Biodiversity Center",
+        "email": "Rob.vanSoest@naturalis.nl",
+        "focus_group": "Sponges",
+        "focus_subgroup": "glass sponges"
+    },
+    {
+        "name": "Christopher Kelley",
+        "organization": "University of Hawaii",
+        "email": "ckelley@hawaii.edu",
+        "focus_group": "Sponges",
+        "focus_subgroup": "glass sponges"
+    },
+    {
+        "name": "Javier Murillo-Perez",
+        "organization": "Fisheries and Oceans Canada",
+        "email": "Javier.MurilloPerez@dfo-mpo.gc.ca",
+        "focus_group": "Sponges",
+        "focus_subgroup": "atlantic sponges"
+    },
+    {
+        "name": "Bruce Mundy",
+        "organization": "NOAA Pacific Islands Fisheries Science Center",
+        "email": "mundyichthyo@gmail.com",
+        "focus_group": "Fishes",
+        "focus_subgroup": "all fishes"
+    },
+    {
+        "name": "Kenneth Sulak",
+        "organization": "United States Geological Survey",
+        "email": "jumpingsturgeon@yahoo.com",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Atlantic"
+    },
+    {
+        "name": "Jorgen Nielsen",
+        "organization": "University of Copenhagen",
+        "email": "jgnielsen@zmuc.ku.dk",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Ophidiiformes"
+    },
+    {
+        "name": "Tomio Iwamoto",
+        "organization": "California Academy of Sciences",
+        "email": "TIwamoto@calacademy.org",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Gadiformes"
+    },
+    {
+        "name": "John McCosker",
+        "organization": "California Academy of Sciences",
+        "email": "JMcCosker@calacademy.org",
+        "focus_group": "Fishes",
+        "focus_subgroup": "eels, BLENNIIFORMES, sharks"
+    },
+    {
+        "name": "Andrea Quattrini",
+        "organization": "Harvey Mudd College",
+        "email": "aquattrini@g.hmc.edu",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Atlantic"
+    },
+    {
+        "name": "Dominique Didier",
+        "organization": "Millersville University",
+        "email": "dominique.didier@millersville.edu",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Chimeras, sharks, rays"
+    },
+    {
+        "name": "David Ebert",
+        "organization": "Moss Landing Marine Laboratories",
+        "email": "debert@mlml.calstate.edu",
+        "focus_group": "Fishes",
+        "focus_subgroup": "sharks"
+    },
+    {
+        "name": "Mackenzie Gerringer",
+        "organization": "University of Hawaii",
+        "email": "mgerring@hawaii.edu",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Liparidae"
+    },
+    {
+        "name": "Astrid Leitner",
+        "organization": "University of Hawaii",
+        "email": "aleitner@hawaii.edu",
+        "focus_group": "Fishes",
+        "focus_subgroup": "all deep fish"
+    },
+    {
+        "name": "Jeff Drazen",
+        "organization": "University of Hawaii",
+        "email": "jdrazen@hawaii.edu",
+        "focus_group": "Fishes",
+        "focus_subgroup": "all fish"
+    },
+    {
+        "name": "Peter Møller",
+        "organization": "Natural History Museum of Denmark",
+        "email": "PDRMoller@snm.ku.dk",
+        "focus_group": "Fishes",
+        "focus_subgroup": "artic fish?"
+    },
+    {
+        "name": "Werner Schwarzhans",
+        "organization": "University of Copenhagen",
+        "email": "wwschwarz@aol.com",
+        "focus_group": "Fishes",
+        "focus_subgroup": "southern ocean"
+    },
+    {
+        "name": "David Smith",
+        "organization": "National Museum of Natural History (Smithsonian)",
+        "email": "smithd@si.edu",
+        "focus_group": "Fishes",
+        "focus_subgroup": "eels"
+    },
+    {
+        "name": "Hsuan Ching (Hans) Ho",
+        "organization": "National Dong Hwa University",
+        "email": "ogcoho@gmail.com",
+        "focus_group": "Fishes",
+        "focus_subgroup": "lophiiformes, all fishes?"
+    },
+    {
+        "name": "Hiroyuki Motomura",
+        "organization": "Kagoshima University",
+        "email": "motomura@kaum.kagoshima-u.ac.jp",
+        "focus_group": "Fishes",
+        "focus_subgroup": "tropical Scorpaenidae (not Sebastidae), Percoidei"
+    },
+    {
+        "name": "Kyoji Fujiwara",
+        "organization": "Kagoshima University",
+        "email": "kyojifujiwara627@yahoo.co.jp",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Lizardfishes and allies, gobies"
+    },
+    {
+        "name": "Yoshiaki Kai",
+        "organization": "Kyoto Univeristy",
+        "email": "mebaru@kais.kyoto-u.ac.jp",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Sebastidae"
+    },
+    {
+        "name": "Nozomu Muto",
+        "organization": "University of Tokai",
+        "email": "nzmutou@tsc.u-tokai.ac.jp",
+        "focus_group": "Fishes",
+        "focus_subgroup": "Sebastidae"
+    }
+]
