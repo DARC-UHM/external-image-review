b465a25437b5a71242dcfd52f6ad20e70681cedb
Rob Godfrey <rob_godfrey@outlook.com>
2026-03-05 20:44:22 -0800

    Add sub qaqc checklist routes

--- application/api/qaqc_checklist/routes.py
+++ application/api/qaqc_checklist/routes.py
@@ -3,6 +3,7 @@ from mongoengine import DoesNotExist
 
 from application.require_api_key import require_api_key
 from application.schema.tator_dropcam_qaqc_checklist import TatorDropcamQaqcChecklist
+from application.schema.tator_sub_qaqc_checklist import TatorSubQaqcChecklist
 from application.schema.vars_qaqc_checklist import VarsQaqcChecklist
 
 from . import qaqc_checklist_bp
@@ -84,7 +85,7 @@ def tator_qaqc_checklist(deployments):
             unique_taxa=0,
             media_attributes=0,
         ).save()
-        current_app.logger.info(f'Created new Tator QA/QC checklist: {deployments}')
+        current_app.logger.info(f'Created new Tator dropcam QA/QC checklist: {deployments}')
     return jsonify(checklist.json()), 200
 
 
@@ -101,5 +102,56 @@ def patch_tator_qaqc_checklist(deployments):
         return jsonify({'error': 'No checklist found for given deployment name'}), 404
     checklist[next(iter(updated_checkbox.keys()))] = next(iter(updated_checkbox.values()))
     checklist.save()
-    current_app.logger.info(f'Updated Tator QA/QC checklist: {deployments}')
+    current_app.logger.info(f'Updated Tator dropcam QA/QC checklist: {deployments}')
+    return jsonify(checklist.json()), 200
+
+
+# get tator sub qaqc checklist (based on transect media id)
+@qaqc_checklist_bp.get('/tator-sub/<transect_media_ids>')
+@require_api_key
+def tator_sub_qaqc_checklist(transect_media_ids):
+    if not transect_media_ids:
+        return jsonify({'error': 'No transect media IDs provided'}), 400
+    try:
+        checklist = TatorSubQaqcChecklist.objects.get(transect_media_ids=transect_media_ids)
+    except DoesNotExist:
+        # create a new checklist
+        checklist = TatorSubQaqcChecklist(
+            transect_media_ids=transect_media_ids,
+            names_accepted=0,
+            missing_qualifier=0,
+            stet_reason=0,
+            tentative_id=0,
+            missing_megahabitat=0,
+            missing_substrate=0,
+            missing_upon=0,
+            upon_substrate=0,
+            suspicious_host=0,
+            time_diff_host_upon=0,
+            missing_ancillary=0,
+            notes_remarks=0,
+            re_examined=0,
+            review_sizes=0,
+            review_exploratory_segments=0,
+            unique_taxa=0,
+            media_attributes=0,
+        ).save()
+        current_app.logger.info(f'Created new Tator sub QA/QC checklist: {transect_media_ids}')
+    return jsonify(checklist.json()), 200
+
+
+# update tator sub qaqc checklist
+@qaqc_checklist_bp.patch('/tator-sub/<transect_media_ids>')
+@require_api_key
+def patch_tator_sub_qaqc_checklist(transect_media_ids):
+    if not transect_media_ids:
+        return jsonify({'error': 'No transect media IDs provided'}), 400
+    updated_checkbox = request.json
+    try:
+        checklist = TatorSubQaqcChecklist.objects.get(transect_media_ids=transect_media_ids)
+    except DoesNotExist:
+        return jsonify({'error': 'No checklist found for given transect media IDs'}), 404
+    checklist[next(iter(updated_checkbox.keys()))] = next(iter(updated_checkbox.values()))
+    checklist.save()
+    current_app.logger.info(f'Updated Tator sub QA/QC checklist: {transect_media_ids}')
     return jsonify(checklist.json()), 200
