3a3daf2dd426d59281d12bd0a4a5ce3d661324a1
Rob Godfrey <rob_godfrey@outlook.com>
2026-03-22 22:17:22 -0700

    Update tator sub qa/qc keys

--- application/schema/tator_sub_qaqc_checklist.py
+++ application/schema/tator_sub_qaqc_checklist.py
@@ -8,16 +8,15 @@ class TatorSubQaqcChecklist(Document):
     missing_qualifier = IntField(required=True, min_value=0, max_value=2)
     stet_reason = IntField(required=True, min_value=0, max_value=2)
     tentative_id = IntField(required=True, min_value=0, max_value=2)
-    missing_megahabitat = IntField(required=True, min_value=0, max_value=2)
-    missing_substrate = IntField(required=True, min_value=0, max_value=2)
     missing_upon = IntField(required=True, min_value=0, max_value=2)
-    upon_substrate = IntField(required=True, min_value=0, max_value=2)
+    upon_not_substrate = IntField(required=True, min_value=0, max_value=2)
     suspicious_host = IntField(required=True, min_value=0, max_value=2)
     time_diff_host_upon = IntField(required=True, min_value=0, max_value=2)
     missing_ancillary = IntField(required=True, min_value=0, max_value=2)
     notes_remarks = IntField(required=True, min_value=0, max_value=2)
     re_examined = IntField(required=True, min_value=0, max_value=2)
     review_sizes = IntField(required=True, min_value=0, max_value=2)
+    review_counts = IntField(required=True, min_value=0, max_value=2)
     review_exploratory_segments = IntField(required=True, min_value=0, max_value=2)
     unique_taxa = IntField(required=True, min_value=0, max_value=2)
     media_attributes = IntField(required=True, min_value=0, max_value=2)
@@ -28,16 +27,15 @@ class TatorSubQaqcChecklist(Document):
             'missing_qualifier': self.missing_qualifier,
             'stet_reason': self.stet_reason,
             'tentative_id': self.tentative_id,
-            'missing_megahabitat': self.missing_megahabitat,
-            'missing_substrate': self.missing_substrate,
             'missing_upon': self.missing_upon,
-            'upon_substrate': self.upon_substrate,
+            'upon_not_substrate': self.upon_not_substrate,
             'suspicious_host': self.suspicious_host,
             'time_diff_host_upon': self.time_diff_host_upon,
             'missing_ancillary': self.missing_ancillary,
             'notes_remarks': self.notes_remarks,
             're_examined': self.re_examined,
             'review_sizes': self.review_sizes,
+            'review_counts': self.review_counts,
             'review_exploratory_segments': self.review_exploratory_segments,
             'unique_taxa': self.unique_taxa,
             'media_attributes': self.media_attributes,
